d54306e83b4ad81aaf519b6a6b88669618490216
Michael <1992mmoore22@gmail.com>
2023-12-04 04:16:23 -0800

    Removed pagination, implemented search

--- src/components/FriendsViewComponent/FriendsView.js
+++ src/components/FriendsViewComponent/FriendsView.js
@@ -3,38 +3,51 @@ import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { URL } from "../../util/url";
 import { MessageBanner } from "../MessageBannerComponent/MessageBanner";
+import SearchIcon from "@mui/icons-material/Search";
 import "./FriendsView.css";
 
 export const FriendsView = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  let [page, setPage] = useState(1);
-  const [friendsList, setFriendsList] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setLoading] = useState(true);
-  const [hasMorePages, setHasMorePages] = useState(false);
+  const [friendsList, setFriendsList] = useState([]);
+  const [searchMode, setSearchMode] = useState(false);
+  const [keyword, setKeyword] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const reqUserId = location.pathname.split("/")[1];
-  const listURL = `${URL}/users/friends/list`;
-  // const searchURL = `${URL}/users/friends/search`;
 
   useEffect(() => {
+    let url;
     setLoading(true);
 
+    if (searchMode)
+      url = `${URL}/users/friends/search?id=${reqUserId}&keyword=${keyword}`;
+    else url = `${URL}/users/friends/list?id=${reqUserId}`;
+
     axios
-      .get(`${listURL}?id=${reqUserId}&page=${page}`)
+      .get(url)
       .then((response) => {
         setFriendsList(response.data.friends);
-
-        if (response.data.lastKey) setHasMorePages(true);
-        else setHasMorePages(false);
-
         setLoading(false);
       })
       .catch((error) => {
         setErrorMessage(error.response.data.message);
       });
-  }, [listURL, reqUserId, page]);
+  }, [searchMode, reqUserId, keyword]);
+
+  const engageSearchMode = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formEntries = Object.fromEntries(formData.entries());
+    const { keyword } = formEntries;
+
+    if (keyword === "" || keyword.trim() === "") setSearchMode(false);
+    else setSearchMode(true);
+
+    setKeyword(keyword);
+  };
 
   return (
     <div className="friendsView">
@@ -44,57 +57,44 @@ export const FriendsView = () => {
         <></>
       )}
 
+      <form className="searchForm" onSubmit={engageSearchMode}>
+        <input
+          name="keyword"
+          className="friendSearch"
+          placeholder="Search..."
+        />
+        <button type="submit" className="submit">
+          <SearchIcon style={{ fontSize: "40px" }} />
+        </button>
+      </form>
+
       {isLoading ? (
-        <div className="loadingMsg">Retrieving this user's friends...</div>
+        <div className="loadingMsg">Retrieving friends...</div>
       ) : (
-        <>
+        <div className="friendList">
           {friendsList.length !== 0 ? (
             <>
               {friendsList.map((friend) => (
-                <div key={friend.resultId} className="listedFriend">
+                <div
+                  key={friend.resultId}
+                  className="listedFriend"
+                  onClick={() => navigate(`/${friend.id}`)}
+                >
                   <div className="friendProfPic">
                     <img
                       className="friendPic"
                       src={friend.pic_url}
                       alt="friend"
-                      onClick={() => navigate(`/${friend.id}`)}
                     />
                   </div>
-                  <div className="friendNameAndOptions">
-                    <div
-                      className="friendName"
-                      onClick={() => navigate(`/${friend.id}`)}
-                    >
-                      {friend.full_name}
-                    </div>
-                  </div>
+                  <div className="friendName">{friend.full_name}</div>
                 </div>
               ))}
-
-              <div className="pageTurners">
-                {page > 1 ? (
-                  <div
-                    className="previous enabled"
-                    onClick={() => setPage(--page)}
-                  >
-                    &#129032;
-                  </div>
-                ) : (
-                  <div className="previous disabled">&#129032;</div>
-                )}
-                {hasMorePages ? (
-                  <div className="next enabled" onClick={() => setPage(++page)}>
-                    &#129034;
-                  </div>
-                ) : (
-                  <div className="next disabled">&#129034;</div>
-                )}
-              </div>
             </>
           ) : (
-            <div className="noFriendsMsg">This user has no friends.</div>
+            <div className="noFriendsMsg">No friends found.</div>
           )}
-        </>
+        </div>
       )}
     </div>
   );
